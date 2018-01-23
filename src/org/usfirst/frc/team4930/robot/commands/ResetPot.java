655b3ef9e0113c75dabbb2d59fd7a70662917f64
Sreerama Jupudy <sreeramajupudy@sreeramas-mbp.nicholsschool.org>
2018-01-23 17:18:13 -0500

    Limit switch reset

--- src/org/usfirst/frc/team4930/robot/commands/ResetPot.java
+++ src/org/usfirst/frc/team4930/robot/commands/ResetPot.java
@@ -1,41 +1,38 @@
 package org.usfirst.frc.team4930.robot.commands;
 
-import org.usfirst.frc.team4930.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class ResetPot extends Command {
+public class ResetPot extends Command
+{
 
-    public ResetPot() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.pot);
-    }
+  public ResetPot() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    // requires(Robot.pot);
+  }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	Robot.pot.reset();
-    }
+  // Called just before this Command runs the first time
+  protected void initialize() {
+    // Robot.pot.reset();
+  }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+  // Called repeatedly when this Command is scheduled to run
+  protected void execute() {}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return true;
-    }
+  // Make this return true when this Command no longer needs to run execute()
+  protected boolean isFinished() {
+    return true;
+  }
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+  // Called once after isFinished returns true
+  protected void end() {}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  protected void interrupted() {
+    end();
+  }
 }
